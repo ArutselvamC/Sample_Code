892c0b33d620bfd510df39eb27f8c97848513a6a
Arutselvam <arutselvam183.c@gmail.com>
2021-09-11 19:21:22 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,16 @@
+let myPonits=3
+
+function add3points()
+{
+ myPonits+=3
+}
+function remove1point()
+
+{
+myPonits-=1
+}
+add3points()
+add3points()
+add3points()
+remove1point()
+console.log(myPonits)
